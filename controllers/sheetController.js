4b64ca17a0e541e72c4aca2ab547cd572c93baa3
KaiqueVOliveira <kaiquevieirao@gmail.com>
2021-06-29 08:17:10 -0300

    logica para exibição das informações concluida

--- controllers/sheetController.js
+++ controllers/sheetController.js
@@ -12,12 +12,7 @@ let sheetController = {
     const spreadsheetId = '1kYrCiRwiXHdOScvTPQGjd7w6NyJgSP_uzXYDuWtShZA';
   
     const googleSheets = google.sheets({ version: 'v4', auth: client});
-  
-    /*const metaData = await googleSheets.spreadsheets.get({
-      auth, 
-      spreadsheetId,
-    })*/
-    
+      
     //cabeçalho de informações da tabela
     const getHeader = await googleSheets.spreadsheets.values.get({
       auth, 
@@ -25,34 +20,62 @@ let sheetController = {
       range:'engenharia_de_software',
     });
 
+    //nome das colunas
     const getColumns = await googleSheets.spreadsheets.values.get({
       auth, 
       spreadsheetId,
       range:'engenharia_de_software!A3:H3',
     });
-    //
+
+    //dados das linhas
     const getTables = await googleSheets.spreadsheets.values.get({
       auth, 
       spreadsheetId,
       range:'engenharia_de_software!A4:H27',
     });
-  
-    let eachTable = getTables.data.values;
-    
-    //console.log(getRows.data.values[3])
-    console.log(eachTable[1])
 
-    let list = [];
+    //separar cada array
+    let eachTable = getTables.data.values;
+
+    let dataList = [];
     let classesN = 60;
     let abscPercentage = classesN/100 * 25;
   
     eachTable.map(function(array){
-      let [reg, name, absence, t1, t2, t3, situation, grade] = array;
+      console.log('separando cada dado do array para atribuir a um objeto')
+      let [reg, name, absence, t1, t2, t3, situation, finalGradeApproval] = array;
+
+      //transformando notas do array em numero inteiro
+      t1 = (parseInt([t1]))
+      t2 = (parseInt([t2]))
+      t3 = (parseInt([t3]))
+
+      let grade = parseInt((t1 + t2 + t3) / 3);
+      let averageGradeMin = 5;
+      let averageGradeMax = 7;
+      let finalExameCalc = parseInt((grade+finalGradeApproval)/2);
+
+      if(averageGradeMin <= grade|| grade < averageGradeMax){
+        situation = 'Exame Final';
+      }
+      if(grade < averageGradeMin){
+        situation = 'Reprovado por Nota';
+      }
       if(absence > abscPercentage){
         situation = 'Reprovado por Falta';
       }
 
-      list.push({
+      if(situation == 'Exame Final'){
+        if(averageGradeMin<=finalExameCalc){
+          finalGradeApproval = finalExameCalc;
+        }
+        else{
+          finalGradeApproval = 0;
+          situation = 'Aprovado';
+        }
+      }
+  
+      dataList.push({
         reg,
         name,
         absence,
@@ -60,13 +83,13 @@ let sheetController = {
         t2,
         t3,
         situation,
-        grade
+        finalGradeApproval
       })
+
     });
     
-
-    res.render('index',{header:getHeader.data, columns:getColumns.data.values, list: list});
-  
+    res.render('index',{header:getHeader.data, columns:getColumns.data.values, list: dataList});
+    console.log('renderizando a view index com as informações para formação da tabela.')
   }
 }
 
